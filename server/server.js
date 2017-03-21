f3bc450862e98e5556d87457018ff314f9660cc8
Paolo Dávila <pdavila@iesebre.com>
2017-03-21 20:38:37 +0100

    Create new Redis instance

--- server/server.js
+++ server/server.js
@@ -3,3 +3,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
+
+// Create new Redis instance
+var redis = new Redis();
